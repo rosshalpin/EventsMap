cba9a986441645f9e58f8f45acecba690489f0b3
Evan Metcalfe <evan.metcalfe2804@hotmail.com>
2017-10-18 16:18:10 +0100

    Email and Password login
    This takes in the email and password that the user inputed, and logs them in if the information matches an existing account. Logout function also made.

---
+++ login.js
@@ -0,0 +1,30 @@
+//Create the login constants
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+
+btnLogin.addEventListener('click', e => {
+    //gets the values from constants
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    //use function to sign in to firebase
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    //if unsuccessful print error to console log
+    promise.catch(e => console.log(e.message));
+});
+
+//This checks if the user is logged in
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        console.log(firebaseUser.email);
+    }else{
+        console.log("Not logged in");
+    }
+})
+
+//This will log the user out
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+
