6cd8b1ff2d902f859a4c044ebb813d5530c1470a
Evan Metcalfe <evan.metcalfe2804@hotmail.com>
2017-10-19 19:41:03 +0100

    Google Signin added
    The ability to sign in using google has been added. When the user signs in using google a token is sent by google and then the user is logged in.

--- Users/login.js
+++ Users/login.js
@@ -1,3 +1,9 @@
+/*<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="client id">
+both lines must be added to html file for google sign in to work.*/
+
+//========================
+//Email and password login
 //Create the login constants
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
@@ -28,3 +34,21 @@ btnLogout.addEventListener('click', e => {
     firebase.auth().signOut();
 });
 
+
+//========================
+//Google Sign in
+//function checks the authentication token given by google once verified creates/logins in the user.
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  var credential =firebase.auth.GoogleAuthProvider.credential(id_token);
+
+  firebase.auth().signInWithCredential(credential).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+});
+}
